8d048a2f313de6481592d696c22749f72b061679
Ziv Zamechek <ziv921@yahoo.com>
2016-08-26 16:39:55 -0400

    Created Chapter model, migration, and association toc ourse

---
+++ db/migrate/20160826203816_create_chapters.rb
@@ -0,0 +1,13 @@
+class CreateChapters < ActiveRecord::Migration
+  def change
+    create_table :chapters do |t|
+      t.integer :course_id      
+      t.string  :name      
+      t.string  :description      
+      t.string  :video_url      
+      t.boolean :completed, default: "false"   
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826191443) do
+ActiveRecord::Schema.define(version: 20160826203816) do
 
   create_table "authentication_tokens", force: :cascade do |t|
     t.string   "body"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160826191443) do
   end
 
   add_index "authentication_tokens", ["user_id"], name: "index_authentication_tokens_on_user_id"
+
+  create_table "chapters", force: :cascade do |t|
+    t.integer  "course_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "video_url"
+    t.boolean  "completed",   default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
