e2258f46d7bb30233fb8aa35bb8bb316ce65586a
Ziv Zamechek <ziv921@yahoo.com>
2016-08-26 15:16:38 -0400

    Added Course Model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826190433) do
+ActiveRecord::Schema.define(version: 20160826191443) do
 
   create_table "authentication_tokens", force: :cascade do |t|
     t.string   "body"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20160826190433) do
   end
 
   add_index "authentication_tokens", ["user_id"], name: "index_authentication_tokens_on_user_id"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "thumbnail"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
