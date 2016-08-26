67a50bf33945e0ef0f5c88f225b7f93e5d82a397
Ziv Zamechek <ziv921@yahoo.com>
2016-08-26 15:13:34 -0400

    Added School Controller with update and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" } 
 
+  resources :schools do 
+  end
+
+  # Users
   get "/my_current_user" => "users#my_current_user"
-
   match 'users/:id' => 'users#update_user', via: [:patch]
-
   get '/send_password' => "users#reset_password"
+  
 end
