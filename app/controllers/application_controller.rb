27cb8185ba02985d25bf1896203b42f9d1e4aad7
Ziv Zamechek <ziv921@yahoo.com>
2016-08-27 09:24:26 -0400

    Added homepage and route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,5 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token  
 
   def index
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "application#index" 
+  root "application#index"
   
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" } 
 
