daeab1643369c623110dfbf8a9000886c62e3ac7
Ziv Zamechek <ziv921@yahoo.com>
2016-08-29 15:25:18 -0400

    Create a search function by filtering cached data

--- app/assets/javascripts/app/controllers/Schools.js
+++ app/assets/javascripts/app/controllers/Schools.js
@@ -2,6 +2,15 @@ angular
   .module("learnplace")
   .controller("IndexController", function($scope, $http) {
     var DATA = [];
+    
+    $scope.query = '';
+
+    $scope.doSearch = function(){
+      $scope.schools = DATA.filter(function(item){
+        return item.name.indexOf($scope.query) != -1;
+      });
+    };
+
     $http.get('/api/v1/schools').then(function successCallback(response) {
         DATA = response.data;
         $scope.schools = DATA;
