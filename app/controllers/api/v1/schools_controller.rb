e1a344f545ba96244af511135b3160770e4abae1
Ziv Zamechek <ziv921@yahoo.com>
2016-08-29 13:03:38 -0400

    Move schools to api folder and add create action

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -1,9 +1,19 @@
-class SchoolsController < ApplicationController
-  before_action :set_school, :except => [:index]
+class Api::V1::SchoolsController < ApplicationController
+  before_action :set_school, :only => [:update, :show]
 
   def index
     @schools = School.all
     render json: @schools
+  end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      render json: { school: @school }, status: 201 
+    else
+      errors = @school.errors.full_messages
+      render json: {errors: errors}, status: 400
+    end
   end
 
   def update
