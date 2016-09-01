a96c5b536552d0c298b6df34f9f969b3e4120f8c
Ziv Zamechek <ziv921@yahoo.com>
2016-08-31 20:28:55 -0400

    Config login and registration routes

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,5 +1,5 @@
 angular
-  .module("learnplace", ['ui.router', 'templates'])
+  .module("learnplace", ['ui.router', 'templates', 'Devise'])
   .config(function ($stateProvider, $urlRouterProvider) {
     console.log("hello world app.js")
     $stateProvider
@@ -8,10 +8,21 @@ angular
           templateUrl: 'app/templates/schools/index.html',
           controller: 'IndexController'
         })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'app/templates/logins/new.html',
+          controller: 'LoginController'
+        })
         .state('school_new', {
           url: '/schools_new',
           templateUrl: 'app/templates/schools/new.html',
           controller: 'NewController'
+        })
+        .state('signup', {
+          url: '/signup',
+          templateUrl: 'app/templates/registrations/new.html',
+          controller: 'RegistrationController'
         });
         $urlRouterProvider.otherwise('/');
+ 
       });
