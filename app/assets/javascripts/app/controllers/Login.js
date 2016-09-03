6c140fa06b9c06e3e6509374795b2e67c86a00eb
Ziv Zamechek <ziv921@yahoo.com>
2016-09-03 17:41:06 -0400

    Clean up a login controller

--- app/assets/javascripts/app/controllers/Login.js
+++ app/assets/javascripts/app/controllers/Login.js
@@ -1,22 +1,12 @@
 angular
   .module("learnplace")
   .controller("LoginController", function($scope, $http, $state, Auth){  
-
     $scope.login = function() {
-      //debugger
       Auth.login($scope.user, { interceptAuth: false }).then(function (response) {
-            $state.go('schools_index')
-            
-        }, function (response) {
-            $scope.error = response.data.error;
-            // $scope.sign_up_status = "alert alert-danger";
-            // $scope.sign_up_message = "Unable to complete registration";
-            // fadeAlert("#sign_up_alert");
-        });
+        $state.go('schools_index')   
+      }, function (response) {
+        $scope.error = response.data.error;
+      });
     }
-
-    // function fadeAlert(id) {
-    //   $(id).fadeTo(3000, 0);
-    // }
   });
   
