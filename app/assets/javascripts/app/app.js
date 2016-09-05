4f2288155c3008ab04408d49b338d0fc71852345
Ziv Zamechek <ziv921@yahoo.com>
2016-09-05 18:20:13 -0400

    Use nested views

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -3,29 +3,39 @@ angular
   .config(function ($stateProvider, $urlRouterProvider) {
     console.log("hello world app.js")
     $stateProvider
-        .state('schools_index', {
-          url: '/',
-          templateUrl: 'app/templates/schools/index.html',
+        .state('home', {
+           url: '/',
+           templateUrl: 'home.html',
+           controller: 'HomeController'
+        })
+        .state('home.schools_index', {
+          url: 'schools',
+          templateUrl: 'schools/index.html',
           controller: 'IndexController'
         })
-        .state('schools_courses', {
-          url: '/schools/:id/courses',
-          templateUrl: 'app/templates/courses/index.html',
+        .state('home.schools_courses', {
+          url: 'schools/:school_id/courses',
+          templateUrl: 'courses/index.html',
           controller: 'CoursesController'
         })
-        .state('login', {
-          url: '/login',
-          templateUrl: 'app/templates/logins/new.html',
+        .state('home.schools_courses_chapters', {
+          url: 'schools/:school_id/courses/:course_id/chapters',
+          templateUrl: 'chapters/index.html',
+          controller: 'ChaptersController'
+        })
+        .state('home.login', {
+          url: 'login',
+          templateUrl: 'logins/new.html',
           controller: 'LoginController'
         })
-        .state('school_new', {
-          url: '/schools_new',
-          templateUrl: 'app/templates/schools/new.html',
+        .state('home.school_new', {
+          url: 'schools_new',
+          templateUrl: 'schools/new.html',
           controller: 'NewController'
         })
-        .state('signup', {
-          url: '/signup',
-          templateUrl: 'app/templates/registrations/new.html',
+        .state('home.signup', {
+          url: 'signup',
+          templateUrl: 'registrations/new.html',
           controller: 'RegistrationController'
         });
         $urlRouterProvider.otherwise('/');
