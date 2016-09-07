193e9458e96bd1c190ed7f2c16bb442ed24b0b39
Ziv Zamechek <ziv921@yahoo.com>
2016-09-07 13:41:40 -0400

    Added logout function and authentication for user signed in

--- app/assets/javascripts/app/controllers/Navbar.js
+++ app/assets/javascripts/app/controllers/Navbar.js
@@ -2,11 +2,26 @@ angular
   .module("learnplace")
   .controller("NavbarController", NavbarController);
 
-function NavbarController($scope, Auth){
+function NavbarController($scope, Auth, $log){
+  
+  $scope.isSignedIn = Auth. isAuthenticated;
+
   Auth.currentUser().then(function(response){
     $scope.email = response.email;
-    $scope.logout = Auth.logout;
+    // $scope.logout = Auth.logout;
+  }, function(){
+    
   }); 
+
+  $scope.logout = function() {
+      alert("you logout");
+
+    Auth.logout().then(function(oldUser) {
+            alert(oldUser.username + "you're signed out now.");
+        }, function(error) {
+            // An error occurred logging out.
+    });
+  }
 
   // $scope.signedIn = function(){
   //     return $scope.email;
