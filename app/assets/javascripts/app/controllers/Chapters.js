dcbbc90cd6eff9679a6f94e5f7bee43dddd5d457
Ziv Zamechek <ziv921@yahoo.com>
2016-09-08 11:58:10 -0400

    Added clear function and debugged chapter service

--- app/assets/javascripts/app/controllers/Chapters.js
+++ app/assets/javascripts/app/controllers/Chapters.js
@@ -3,18 +3,22 @@
   angular 
     .module("learnplace")
     .controller("ChaptersController", ChaptersController)
-  
 
   function ChaptersController($scope, $http, $state, $stateParams, SchoolService, CourseService, ChapterService) {
     
     var DATA = [];
     var endpoint = '/api/v1/schools/' + $stateParams.school_id + '/courses/' + $stateParams.course_id;
 
-    $scope.chapter = {
-      name: '',
-      description: ''
-    };
+    function clear(){
+      $scope.chapter = {
+        name: '',
+        description: ''
+      };
+    }
 
+    clear();
+
+    $scope.school_id = $stateParams.school_id;
     $scope.errors = [];
     $scope.query = '';
     $scope.school_name = '';
@@ -33,8 +37,11 @@
 
 
     $scope.create = function() {
-      ChapterService.create($scope.chapter).then(function(result){
-        //$state.go('home.schools_courses_chapters');
+      ChapterService.create($stateParams.school_id, $stateParams.course_id, $scope.chapter).then(function(result){
+        console.log("RESULTS", result.data.name);
+        DATA.push(result.data);
+        clear();
+        $state.go('home.schools_courses_chapters');
       }, function(response){
         $scope.errors = response.data.errors;
       });
@@ -51,12 +58,9 @@
      $scope.course_name = response.data.name;
     });
 
-    // ChapterService.get($stateParams.chapter_id, $stateParams.course_id, $stateParams.school_id).then(function(response) {
-    //   $scope.chapter_name = response.data.name;
-    // });
-
-
-    ChapterService.list().then(function(response) {
+    ChapterService.list($stateParams.course_id, $stateParams.school_id).then(function(response) {
+      console.log("response", response);
+      console.log(" controllers $stateParams", $stateParams);
         DATA = response.data;
         $scope.chapters = DATA;
       }, function(response) {
