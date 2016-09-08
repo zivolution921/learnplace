63313d8f8d3759684df36f2837b57f1966691252
Ziv Zamechek <ziv921@yahoo.com>
2016-09-08 11:11:40 -0400

    Added resolve to schools and courses
    for prefetching data

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -12,12 +12,25 @@ angular
         .state('home.schools_index', {
           url: 'schools',
           templateUrl: 'schools/index.html',
-          controller: 'IndexController'
+          controller: 'IndexController',
+          resolve: {
+            schools: function(SchoolService){ 
+              return SchoolService.list();
+            }
+          }
         })
         .state('home.schools_courses', {
           url: 'schools/:school_id/courses',
           templateUrl: 'courses/index.html',
-          controller: 'CoursesController'
+          controller: 'CoursesController',
+          resolve: {
+            school: function($stateParams, SchoolService){
+              return SchoolService.get($stateParams.school_id);
+            },
+            courses: function($stateParams, CourseService) {
+              return CourseService.list($stateParams.school_id);
+            }
+          }
         })
         .state('home.schools_courses_chapters', {
           url: 'schools/:school_id/courses/:course_id/chapters',
